a535a00b6dc805f72672b46a9f1cfc810df9c762
asokol <sokolik.andr@gmail.com>
2020-04-26 16:36:08 +0300

    first commit

---
+++ src/Loader.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default () => (
+  <div>
+    <div
+      style={{ display: "flex", justifyContent: "center", margin: "05rem" }}
+      className="lds-dual-ring"
+    ></div>
+  </div>
+);
